e6843038e36b8e83c1f49289307246311e05f048
Laner <iifunnymomen@gmail.com>
2020-08-10 13:40:41 +0300

    feature/ISTAT-1321-oauth-v2

--- lib/amorail/client.rb
+++ lib/amorail/client.rb
@@ -2,16 +2,87 @@ require 'faraday'
 require 'faraday_middleware'
 require 'json'
 require 'active_support'
+require 'oauth2'
+
+OAUTH_ENDPOINT = 'https://www.amocrm.ru'
+API_HOST = 'https://istat24.amocrm.ru'
 
 module Amorail
   # Amorail http client
   class Client
-    attr_reader :usermail, :api_key, :api_endpoint, :custom_options
+    attr_reader :usermail, :api_key, :api_endpoint, :custom_options, :client_id, :client_secret, :redirect_uri
+    attr_accessor :access_token
+    #def initialize(attrs = {})
+      #@endpoint = attrs[:endpoint]
+    #  @access_token = attrs[:access_token]
+    #  @client_id = attrs[:client_id]
+    #  @client_secret = attrs[:client_secret]
+    #  @redirect_uri = attrs[:redirect_uri]
+      #@scope = attrs[:scope]
+    #end
+
+    def oauth2client
+      @_oauth2client ||=
+        OAuth2::Client.new(
+          client_id,
+          client_secret,
+          site: OAUTH_ENDPOINT,
+          authorize_url: '/oauth',
+          token_url: '/oauth2/access_token'
+        )
+    end
+
+    def authorize_url
+      return nil unless oauth2client
+
+      oauth2client.auth_code.authorize_url(
+        redirect_uri: redirect_uri
+      )
+    end
+
+    def fetch_access_token(code)
+      return unless oauth2client
+      @_oauth2client.site = API_HOST
+
+      token = oauth2client.auth_code.get_token(code, redirect_uri: redirect_uri)
+      token.params.merge(
+        access_token: token.token,
+        refresh_token: token.refresh_token,
+        expires_at: token.expires_at,
+        expires_in: token.expires_in
+      )
+    rescue OAuth2::Error => e
+      puts e #FIXME if ::Amorail.debug
+    end
+
+    def fetch_refresh_token(refresh_token)
+      return unless oauth2client
+
+      token = oauth2client.get_token(
+        refresh_token: refresh_token,
+        grant_type: 'refresh_token'
+      )
+      token.params.merge(
+        access_token: token.token,
+        refresh_token: token.refresh_token,
+        expires_at: token.expires_at
+      )
+    rescue OAuth2::Error => e
+      puts e #FIXME if  ::Amorail.debug
+    end
 
     def initialize(api_endpoint: Amorail.config.api_endpoint,
                    api_key: Amorail.config.api_key,
                    usermail: Amorail.config.usermail,
+                   access_token: Amorail.config.access_token,
+                   client_id: Amorail.config.client_id,
+                   client_secret: Amorail.config.client_secret,
+                   redirect_uri: Amorail.config.redirect_uri,
                    custom_options: {})
+      @access_token = access_token
+      @client_id = client_id
+      @client_secret = client_secret
+      @redirect_uri = redirect_uri
       @api_endpoint = api_endpoint
       @api_key = api_key
       @usermail = usermail
@@ -51,9 +122,10 @@ module Amorail
     def get(url, params_in = {})
       headers = params_in[:headers]
       params  = params_in.clone
-      params.delete(:headers)      
+      params.delete(:headers)
       response = connect.get(url, params) do |request|
         request.headers['Cookie'] = cookies if cookies.present?
+        request.headers['Authorization'] = "Bearer #{access_token}"
         headers&.each { |k, v| request.headers[k.to_s] = v.to_s }
       end
       handle_response(response)
@@ -66,6 +138,7 @@ module Amorail
       response = connect.post(url) do |request|
         request.headers['Cookie'] = cookies if cookies.present?
         request.headers['Content-Type'] = 'application/json'
+        request.headers['Authorization'] = "Bearer #{access_token}"
         headers&.each { |k, v| request.headers[k.to_s] = v.to_s }
         request.body = params.to_json
       end
